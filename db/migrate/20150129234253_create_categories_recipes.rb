22322226585c6266707809819d2d715c28ea808c
Clem Freeman and Phil Orchard <philiporchard@gmail.com>
2015-01-29 15:50:48 -0800

    Add instructions and headers to recipe.erb

---
+++ db/migrate/20150129234253_create_categories_recipes.rb
@@ -0,0 +1,16 @@
+class CreateCategoriesRecipes < ActiveRecord::Migration
+  def change
+    create_table(:categories) do |t|
+      t.column :name, :string
+
+      t.timestamps
+    end
+
+    create_table(:categories_recipes) do |t|
+      t.column :category_id, :integer
+      t.column :recipe_id, :integer
+
+      t.timestamps
+    end
+  end
+end
